3216b0cd66f33108ba90a8a7394284d7caf48529
Sima Mara <marasima04@gmail.com>
2020-12-30 16:30:44 +0200

    added the book feature

--- library.py
+++ library.py
@@ -3,6 +3,20 @@ class Library:
         self.capacity = capacity
         self.users = []
 
+    class Book:
+        def __init__(self,maxBooks):
+            self.maxBooks = maxBooks
+            self.books = []
+
+        def free_space(self):
+            return self.maxBooks - len(self.books)
+
+        def add_book(self,title):
+            if not self.free_space():
+                return False
+            self.books.append(title)
+            return True
+
     def add_user(self,name):
         if not self.free_spots():
             return False
@@ -15,6 +29,9 @@ class Library:
 library = Library(2)
 
 users = ["Anita" , "Anda", "Teo"]
+books1 = ["Maitrey" , "Lord of The Flies", "Crime and Punishment"]
+books2 = ["Papillon", "Hamlet"]
+books3 = ["The Boy in striped pajamas"]
 
 for user in users:
     if library.add_user(user):
